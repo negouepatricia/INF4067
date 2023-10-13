8b910eff4901e705262084fa336e42ddcec67f74
negouepatricia <negouepatricia@gmail.com>
2023-10-13 20:12:29 +0100

    Ajouter Builder message

---
+++ Builder/monteurpizza.java
@@ -0,0 +1,22 @@
+abstract class MonteurPizza {
+ protected Pizza pizza;
+public Pizza getPizza() { return pizza; }
+ public void creerNouvellePizza() { pizza = new Pizza(); }
+ public abstract void monterPate();
+ public abstract void monterSauce();
+ public abstract void monterGarniture();
+}
+
+class MonteurPizzaReine extends MonteurPizza {
+ public void monterPate() { pizza.setPate("croisée"); }
+ public void monterSauce() { pizza.setSauce("douce"); }
+ public void monterGarniture() { 
+pizza.setGarniture("jambon+champignon"); }
+}
+
+class MonteurPizzaPiquante extends MonteurPizza {
+ public void monterPate() { pizza.setPate("feuilletée"); }
+ public void monterSauce() { pizza.setSauce("piquante"); }
+ public void monterGarniture() { pizza.setGarniture("pepperoni+salami"); 
+}
+}
